ce461c1c0978013be9ca7577299b109dd2fc4533
juliana.oliveira <juliana.oliveira@tallos.com.br>
2023-02-14 16:20:54 -0300

    component to search pokemon by name or id

---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h3>Pokemons</h3>
+    <InputPokemon v-model="pokemon.name" />
+    <button @click="findPokemonByIdOrName(pokemon.name)">Search</button>
+
+<div>
+  <Card />
+</div>
+     </div>
+</template>
+
+<script lang="ts">
+import { ApiService } from '@/service/api';
+import { defineComponent } from 'vue';
+import InputPokemon from '@/components/InputSearch.vue'
+export default defineComponent({
+  name: 'AllPokemonsList',
+  components:{
+    InputPokemon,
+  },
+  data() {
+    const apiService = new ApiService()
+    return { apiService, pokemon: {name: "", }}
+  },
+  methods: {
+    async findPokemonByIdOrName(param: string) {
+        const response = await this.apiService.findPokemonByName(param)
+        return this.pokemon = response.data;
+
+      },
+      },
+})
+
+
+</script>
+<style lang="scss">
+
+</style>
