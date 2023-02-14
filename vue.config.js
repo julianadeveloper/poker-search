0f15b6067ce959cad63b9557a78cb1dce9969421
juliana.oliveira <juliana.oliveira@tallos.com.br>
2023-02-14 16:18:07 -0300

    sass to style global and pokemon-card  and config vue to sass

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: 
-         `@import "../scss/global.scss";`
+  // css: {
+  //   loaderOptions: {
+  //     sass: {
+  //       additionalData: 
+  //        `@import "@/scss/global.scss";`
 
-      }
-    }
-  }
+  //     }
+  //   }
+  // }
 })
 
 
