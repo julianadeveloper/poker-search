1f03fc5309ba66189483564b602025445a79fc65
juliana.oliveira <juliana.oliveira@tallos.com.br>
2023-02-14 16:19:09 -0300

    create input component

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="Wrapper">
+        <label for="input" class="Input-label">Buscar Pokémon</label>
+        <input :type="type" :value="modelValue" @input="searchValue"  id="input" class="Input-text" />
+        
+      </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BaseInput',
+  props:{
+  type: String || Number,
+  modelValue: [String, Number]
+ },
+ methods:{
+  searchValue(event: any){
+   this.$emit('update:modelValue', event.target.value)
+  }
+ }
+ })
+
+</script>
